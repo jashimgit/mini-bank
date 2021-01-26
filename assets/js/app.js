09f2304fd843d578897ebdfbf6c174dd1a866d53
jashimgit <jashim.ncn@gmail.com>
2021-01-26 18:47:21 +0600

    deposite system and widthdraw system done and total balance working now

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,58 @@ const loginFormCard = document.getElementById('login-form-card');
 const transactionArea = document.getElementById('transaction-area');
 // hide login area with button event
 
-loginBtn.addEventListener('click', function() {
+loginBtn.addEventListener('click', function () {
     loginArea.style.display = 'none';
     // console.log('clicked');
-    transactionArea.style.display = 'block'
+    transactionArea.style.display = 'block';
 });
+
+
+const depositeBtn = document.getElementById('deposite-btn');
+
+
+// add deposite button event handler
+depositeBtn.addEventListener('click', function () {
+    // get deposite input value
+    let currentDeposit = parseFloat(document.getElementById('deposit-amount').value);
+    // get deposited amount from deposite area
+    let totalCurrentDeposit = parseFloat(document.getElementById('current-deposit').innerText);
+    totalCurrentDeposit += currentDeposit;
+    // update total deposited amount
+    document.getElementById('current-deposit').innerText = totalCurrentDeposit;
+    // set deposite input box value to empty after submit deposite
+    document.getElementById('deposit-amount').value = "";
+
+    // get total deposit from 
+    let totalBalance = parseFloat(document.getElementById('total-balance').innerText);
+
+    totalBalance += totalCurrentDeposit;
+    // update total balance after deposit
+    document.getElementById('total-balance').innerText = totalBalance;
+});
+
+// widthdraw button event handler 
+
+const widthDrawBtn = document.getElementById('widthdraw-btn');
+widthDrawBtn.addEventListener('click', function () {
+    // get totalBalance 
+    let totalBalance = parseFloat(document.getElementById('total-balance').innerText);
+    // get total Widthdraw amount
+    let widthdrawBalance = parseFloat(document.getElementById('widthdraw-balance').innerText);
+    // get widthdraw input value
+    let widthDrawValue = parseFloat(document.getElementById('width-amount').value);
+    // console.log(widthDrawValue);
+    widthdrawBalance += widthDrawValue;
+    // update total widthdraw balancce
+    document.getElementById('widthdraw-balance').innerText = widthdrawBalance;
+    // update widthdraw input value to empty after widthdraw
+    document.getElementById('width-amount').value = "";
+
+    // update total Balace after widthdraw
+    updateTotalBalance(totalBalance, widthDrawValue);
+})
+
+function updateTotalBalance(totalBalance, widthDrawValue) {
+    let updatedTotalBalance = totalBalance - widthDrawValue;
+    document.getElementById('total-balance').innerText = updatedTotalBalance;
+}
